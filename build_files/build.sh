90e10193d39c76b8111df2e0898ba402ade6e542
rpassmore <rpassmore@gmail.com>
2025-05-04 20:03:17 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,12 +18,12 @@ dnf5 -y copr enable che/nerd-fonts
 dnf5 -y copr enable atim/starship
 
 # install new kernel & remove old one
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
+#dnf5 -y copr enable bieszczaders/kernel-cachyos
+#dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 #dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 #dnf5 -y install kernel-cachyos kernel-cachyos-devel-matched scx-scheds
-dnf5 -y copr disable bieszczaders/kernel-cachyos
-dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
+#dnf5 -y copr disable bieszczaders/kernel-cachyos
+#dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
 
 # this installs a package from fedora repos
 dnf5 -y install \
