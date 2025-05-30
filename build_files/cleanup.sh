6a3db2612fa63df8cd41acd5ca5f9384b7b9748b
rpassmore <rpassmore@gmail.com>
2025-05-30 14:14:26 +0100

    install vs code as system app

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -2,5 +2,12 @@
 
 set -eoux pipefail
 
+dnf5 -y copr disable ublue-os/packages
+dnf5 -y copr disable che/nerd-fonts
+dnf5 -y copr disable atim/starship
+dnf5 -y config-manager setopt terra.enabled=0
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/vscode.repo
+
 dnf5 clean all
 rm -rf /tmp/* || true
